b14ede4eaefdfd70eeabe136f84d25e48b94d55c
aswi <aswi@mail.ru>
2024-01-28 22:26:11 +0300

    Libs

---
+++ ESP8266AutoWifi/examples/AutoConnect/AutoConnect.ino
@@ -0,0 +1,18 @@
+#include <ESP8266AutoWifi.h>
+
+ESPAutoWifi wf;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  // uncomment below for testing
+  // wf.reset();
+
+  //starting autoconnect wifi system
+  wf.autoConnect();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
